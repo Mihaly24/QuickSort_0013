7405168bfa8070ae03144fa2bd57715820f3291f
Mihaly24 <fairuzk20@gmail.com>
2024-03-28 17:43:26 +0700

    Menambahkan bagian while pada input

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -6,6 +6,14 @@ int cmp_count = 0;
 int mov_count = 0;
 int n;
 
+void input() {
+    while (true)
+    {
+        cout << "Masukkan panjang element array: ";
+        cin >> n;
+    }
+}
+
 int main()
 {
     
